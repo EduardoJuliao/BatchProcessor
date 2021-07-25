57599594e1ca3129640b9748936627bec3ba7a97
Eduardo julião <eduardo.juliaojr@outlook.com>
2021-07-25 11:34:47 +0100

    web app project

--- BatchProcessor.WebApp/Pages/Error.cshtml.cs
+++ BatchProcessor.WebApp/Pages/Error.cshtml.cs
@@ -1,7 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
-using System.Diagnostics;
 
 namespace BatchProcessor.WebApp.Pages
 {
